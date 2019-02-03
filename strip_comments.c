e55504cb35d54b43278046510c3d4a77af628514
Diego Codevilla <diegocode@gmail.com>
2019-02-03 13:21:02 -0300

    Removed printf used for debug. Add comments

--- strip_comments.c
+++ strip_comments.c
@@ -14,6 +14,7 @@
 
 int main()
 {
+    // states used for the state-machine
     enum strip_comments_states{
         write,
         slash,
@@ -22,43 +23,62 @@ int main()
         multi_stop
         } state;
     
+    // state variable - initial state: write
     state = write;
     
+    // file handlers fin: input file / fout: output file
     FILE *fin;
     FILE *fout;
-    
-    char line[180];
-    char origin_name[] = "test.c";
+   
+    char origin_name[] = "test.c";     
     char destination_name[] = "test.noc.c";
     
+    // open input file & check errors 
     fin = fopen(origin_name, "r");
     if (fin == NULL) {
         printf("Error opening %s or file doesn't exists\n", origin_name);
         return 1;
     }
     
+    // open output file & check errors 
     fout = fopen(destination_name, "w");
     if (fout == NULL) {
         printf("Error opening %s or file doesn't exists\n", destination_name);
         return 2;        
     }
     
+    // loop until end of file...
     char c;
     while (!feof(fin)) {        
+        // read one char from file
         c = fgetc(fin);
         
+        // state machine..
         switch(state) {
             default:
-            case write:
+            case write:         
+                /* 
+                write: 
+                - if char is '/' next state is "slash"
+                - if char isn't '/' writes char to output file
+                */
                 if (c == '/') {
                     state = slash;
-                } else {
-                    printf("%c", c);
+                } else {              
                     fputc(c, fout);
                 }
                 break;
             
             case slash:
+                /* 
+                slash: 
+                - if char is '/' => it's a single-line comment
+                  next state is "single_start"
+                - if char is '*' => it's a multiline comment start
+                  next state is "multi_start"
+                - if char isn't '/' nor '*' => previous '/' was't the beginning 
+                  of a comment, so writes previous '/' and currenr char to output file
+                */
                 if (c == '/') {
                     state = single_start;
                 } else 
@@ -66,47 +86,55 @@ int main()
                     state = multi_start;
                 } else {
                     state = write;
-                    printf("%c", '/');
                     fputc('/', fout);                  
-                    printf("%c", c);
                     fputc(c, fout);                  
                 }
-                
                 break;
             
             case single_start:
+                /* 
+                single_start: 
+                - if char is new line => single-line comment ends.
+                  next state is "write"
+                - if char is tab, newline or carriage return, writes to output
+                */                
                 if (c == '\n') {
                     state = write;
                 }
                 
                 if ((c == '\n') || (c == '\r') || (c == '\t')) {
-                    printf("%c", c);
                     fputc(c, fout);
-                }
-                
+                }                
                 break;
                 
             case multi_start:
+                /* 
+                multi_start: 
+                - if char is '*' => possible multi-line comment end
+                  next state is "multi-stop"
+                */  
                 if (c == '*') {
                     state = multi_stop;
-                }
-                                    
+                }        
                 break;
                 
             case multi_stop:
+                /* 
+                multi_stop: 
+                - if char is '/' => found multi-line comment end
+                  next state is "write"
+                - if char isn't '/' next state is multi_start
+                */  
                 if (c == '/') {
                     state = write;
                 } else {
                     state = multi_start;
                 }
-                
                 break;
             }
-            
-                
-                    
         }  
 
+    // close files
     fclose(fout);
     fclose(fin);    
     
